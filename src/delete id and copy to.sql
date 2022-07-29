e510fea5cb0285cad04db0e512e5f6a310abbb38
dravven <danild98@gmail.com>
2022-07-29 16:03:06 +0300

    last

--- src/delete id and copy to.sql
+++ src/delete id and copy to.sql
@@ -4,9 +4,7 @@ create table if not exists user_order_log_2(
 
 create table if not exists pre_user_order_log
 (
-    id             serial    not null
-        constraint user_order_log_pkey
-            primary key,
+    id             int
     date_time      timestamp not null,
     city_id        integer   not null,
     city_name      varchar(100),
